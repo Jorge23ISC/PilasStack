fd5783e9568e80a7309688b221e19977013775ff
Jorge23ISC <jorgejuarez@gmail.com>
2024-09-24 16:47:20 -0600

    Ejercicio de Pilas con Stack

---
+++ BuscadorPila.java
@@ -0,0 +1,58 @@
+package Pilas;
+import java.util.Stack;
+public class BuscadorPila {
+    private int capacidad, cima;
+    Stack<String> pila = new Stack<>();
+    String info = "";
+
+    public BuscadorPila(int capacidad) {
+        this.capacidad = capacidad;
+        this.cima = -1;
+    }
+
+    public void agregar(String info) {
+        if (lleno()) {
+            System.out.println("El Historial se encuentra lleno");
+            return;
+        }
+        cima++;
+        pila.push(info);
+        System.out.println("La busqueda '" + info + "' ha sido almacenada");
+    }
+
+    public String quitar() {
+        if (vacio()) {
+            System.out.println("El Historial se encuentra vacio");
+            return null;
+        }
+        info = pila.pop();
+        cima--;
+        System.out.println("La busqueda '" + info + "' ha sido eliminada");
+        return info;
+    }
+
+    public String Limite() {
+        if (vacio()) {
+            System.out.println("El Historial se encuentra vacio");
+            return null;
+        }
+        System.out.println(pila.peek());
+        return pila.peek();
+    }
+
+    public void mostrarPila() {
+        if (vacio()) {
+            System.out.println("El Historial se encuentra vacio");
+            return;
+        }
+        System.out.println(pila);
+    }
+
+    public boolean vacio() {
+        return cima == -1;
+    }
+
+    public boolean lleno() {
+        return cima == capacidad - 1;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,54 @@
+package Pilas;
+import java.util.Scanner;
+//Andres Manuel Flores Bonilla
+//Jimena Hernandez Luna
+//Jorge Juarez Marcelo
+//Sebastian Gadiel Perez Solano
+public class Main {
+    public static void main(String[] args) {
+        buscadorStack();
+    }
+    public static void buscadorStack() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la capacidad de busqueda: ");
+        int capacidad = scanner.nextInt();
+        scanner.nextLine();
+        BuscadorPila buscador = new BuscadorPila(capacidad);
+        int opcion;
+
+        do {
+            System.out.println("\nOperaciones:");
+            System.out.println("1. Ingresar Busqueda");
+            System.out.println("2. Eliminar Busqueda");
+            System.out.println("3. Ver cima (peek)");
+            System.out.println("4. Mostrar Historial");
+            System.out.println("5. Salir");
+            System.out.println("Seleccione una opcion");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese Su Busqueda: ");
+                    String elemento = scanner.nextLine();
+                    buscador.agregar(elemento);
+                    break;
+                case 2:
+                    buscador.quitar();
+                    break;
+                case 3:
+                    buscador.Limite();
+                    break;
+                case 4:
+                    buscador.mostrarPila();
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("No valido");
+            }
+        } while (opcion != 5);
+        scanner.close();
+    }
+}
+
